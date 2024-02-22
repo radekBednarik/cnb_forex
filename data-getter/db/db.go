9b6220838c02ae328695b8c9736cdfb9b4ffc3e8
radek.Bednarik <radek.bednarik@tesena.com>
2024-02-22 15:09:50 +0100

    dev: wip

--- data-getter/db/db.go
+++ data-getter/db/db.go
@@ -3,6 +3,7 @@ package db
 // https://pkg.go.dev/github.com/jackc/pgx/v5/pgxpool#pkg-overview
 import (
 	"context"
+	"fmt"
 	"log"
 
 	pgpool "github.com/jackc/pgx/v5/pgxpool"
@@ -63,34 +64,44 @@ func CreateTables(dbs Database) {
 
 	// if the db is empty, create tables
 	if !result {
+		// ensure, that primary uuid keys are always automatically created by default
+		statement := "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
+		conn = dbs.connect()
+		_, err := conn.Exec(context.Background(), statement)
+		conn.Release()
+
+		if err != nil {
+			log.Fatalf("Creating uuid-ossp extension failed with error: %v", err)
+		}
+
 		statements := []string{
 			`-- Creating the country table
 CREATE TABLE country (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR UNIQUE NOT NULL
 );
 `,
 			`-- Creating the curr_name table
 CREATE TABLE curr_name (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR UNIQUE NOT NULL
 );
 `,
 			`-- Creating the curr_symbol table
 CREATE TABLE curr_symbol (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     symbol VARCHAR UNIQUE NOT NULL
 );
 `,
 			`-- Creating the date table
 CREATE TABLE date (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     date DATE UNIQUE NOT NULL
 );
 `,
 			`-- Creating the data table with foreign key constraints
 CREATE TABLE data (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     country_id UUID REFERENCES country(id),
     curr_name_id UUID REFERENCES curr_name(id),
     curr_symbol_id UUID REFERENCES curr_symbol(id),
@@ -111,4 +122,84 @@ CREATE TABLE data (
 		}
 
 	}
+}
+
+func InsertIntoCountry(value string, dbs Database) {
+	conn := dbs.connect()
+
+	qString := fmt.Sprintf("INSERT INTO country (name) VALUES (%s) ON CONFLICT (name) DO NOTHING;", value)
+
+	_, err := conn.Exec(context.Background(), qString)
+	conn.Release()
+
+	if err != nil {
+		log.Fatalf("Inserting data to country table failed with error: %v\n", err)
+	}
+}
+
+func InsertIntoCurrName(value string, dbs Database) {
+	conn := dbs.connect()
+
+	qString := fmt.Sprintf("INSERT INTO curr_name (name) VALUES (%s) ON CONFLICT (name) DO NOTHING;", value)
+
+	_, err := conn.Exec(context.Background(), qString)
+	conn.Release()
+
+	if err != nil {
+		log.Fatalf("Inserting data to curr_name table failed with error: %v\n", err)
+	}
+}
+
+func InsertIntoCurrSymbol(value string, dbs Database) {
+	conn := dbs.connect()
+
+	qString := fmt.Sprintf("INSERT INTO curr_symbol (symbol) VALUES (%s) ON CONFLICT (symbol) DO NOTHING;", value)
+
+	_, err := conn.Exec(context.Background(), qString)
+	conn.Release()
+
+	if err != nil {
+		log.Fatalf("Inserting data to curr_symbol table failed with error: %v\n", err)
+	}
+}
+
+func InsertIntoDate(value string, dbs Database) {
+	conn := dbs.connect()
+
+	qString := fmt.Sprintf("INSERT INTO date (date) VALUES (%s) ON CONFLICT (date) DO NOTHING;", value)
+
+	_, err := conn.Exec(context.Background(), qString)
+	conn.Release()
+
+	if err != nil {
+		log.Fatalf("Inserting data to date table failed with error: %v\n", err)
+	}
+}
+
+func SelectIdFromTable(value string, fieldName string, table string, dbs Database) (string, error) {
+	conn := dbs.connect()
+
+	qString := fmt.Sprintf("SELECT id from %s WHERE %s = '%s' LIMIT 1;", table, fieldName, value)
+
+	res := conn.QueryRow(context.Background(), qString)
+
+	var result string
+	err := res.Scan(&result)
+
+	conn.Release()
+
+	return result, err
+}
+
+func InsertIntoData(country string, curr_name string, curr_symbol string, date string, value float64, dbs Database) {
+	conn := dbs.connect()
+
+	// check, if date is already in the date table
+	// if there is no row, then error means insert into the table
+	id, err := SelectIdFromTable(date, "date", "date", dbs)
+	if err != nil {
+		InsertIntoDate(date, dbs)
+	}
+
+	// TODO: do checks for all other values and insert them, if they are not present
 }
