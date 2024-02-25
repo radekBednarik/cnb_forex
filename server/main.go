b5699be0b2042c128f4b22da13fc7521e61a6550
radek.Bednarik <radek.bednarik@tesena.com>
2024-02-25 15:31:24 +0100

    dev: added hello world basic server via gin

--- server/main.go
+++ server/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	g := gin.Default()
+	g.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "hello, world!"})
+	})
+	g.Run()
+}
