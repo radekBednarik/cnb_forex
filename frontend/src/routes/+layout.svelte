c47d356ca2d2ebc3ec2b0b7e7537bfb1def6eab6
radekBednarik <radek.bednarik@tesena.com>
2024-03-06 19:04:13 +0100

    dev: added flowbite lib and started with dashboard

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.pcss';
+	import { Hr, Heading, Span} from "flowbite-svelte";
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+
+<header>
+	<Heading tag='h1'><Span gradient>Daily forex data</Span></Heading>
+	<Heading tag='h4'>
+		provided by <a
+			href="https://www.cnb.cz"
+			target="_blank"
+			referrerpolicy="no-referrer-when-downgrade">Czech national bank</a
+		>
+	</Heading>
+	<Hr/>
+</header>
+<slot />
