379985a38a755a794fb5e5275a3238269e7a46cf
Radek Bednarik <radek.bednarik@tesena.com>
2024-03-21 10:14:25 +0100

    config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -9,6 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		csp: {
+			mode: "auto",
 			directives: {
 				'script-src': ['self', 'unsafe-inline'],
 				'object-src': ['none'],
